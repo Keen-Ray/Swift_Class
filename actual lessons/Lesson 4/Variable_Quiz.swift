ef4b08236fb85140de66b50355fb53a17743c71d
Keenan Rhea <keenanrhea@gmail.com>
2016-02-14 08:41:31 -0600

    Add a quiz on variables to Lesson 4

---
+++ actual lessons/Lesson 4/Variable_Quiz.swift
@@ -0,0 +1,3 @@
+let r: Double = 10
+let pi: Double = 3.14
+print(pi*r*r)
---
+++ actual lessons/Lesson 4/Variable_doc.swift
@@ -0,0 +1,17 @@
+
+//: ##Math Operators
+var name = value * value2
+name = value - value2
+name = value + value2
+name = value / value2
+name = value % value2
+//: ## Variables
+let variable_name = value
+var changeable_variable_name = value
+changeable_variable_name = new_value
+//: # Possible values for variables
+var type_Double = 3.14159265359
+var type_Double: Double = 10
+/*: ## Your Assignment
+Make a program that prints the area of a circle. Make sure that this program incorporates a variable that holds the value of the radius.
+*/
