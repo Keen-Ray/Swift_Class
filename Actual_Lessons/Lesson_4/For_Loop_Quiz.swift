2f61af19c5333e20923cfd4a9e14bcff1377d269
Keenan Rhea <keenanrhea@gmail.com>
2016-02-29 20:28:19 -0600

    Add a while loop version to quiz

--- Actual_Lessons/Lesson_4/For_Loop_Quiz.swift
+++ Actual_Lessons/Lesson_4/For_Loop_Quiz.swift
@@ -5,3 +5,12 @@ for var counter = 0; counter+1 < to_the_power_of; counter++ {
   number = number * number_copy
 }
 print(number)
+
+var to_the_power_of = 7
+var number = 3
+var number_copy
+var counter = 0
+while counter+1< to_the_power_of{
+  number = number * number_copy
+  counter++
+}
